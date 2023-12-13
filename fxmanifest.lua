64b766798b5ee80c5ae550d839fecf5147d86177
TehSteel <50503589+TehSteel@users.noreply.github.com>
2023-12-13 17:29:23 +0200

    refactor(fxmanifest)
    Fixed config.lua could be dumped with the webhook

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,11 @@
-fx_version "adamant"
-game "gta5"
-use_fxv2_oal "yes"
-lua54 "yes"
-
-shared_scripts { 
-    '@es_extended/imports.lua',
-    'config.lua'
-}
+fx_version 'adamant'
+game 'gta5'
+use_fxv2_oal 'yes'
+lua54 'yes'
+server_only 'yes'
 
 server_scripts { 
+    '@es_extended/imports.lua',
     '@oxmysql/lib/MySQL.lua', 
     'server/*.lua',
     'config.lua'
